07a96b6dc633959246836c93025986c83d992c30
KevinDupeyrat <k.dupeyrat@protonmail.com>
2018-03-26 15:04:53 +0200

    Initialisation

---
+++ Projet-Partie-ClientManager/Projet-Partie-ClientManager/Controllers/ClientController.cs
@@ -0,0 +1,39 @@
+﻿using Projet_Partie_ClientManager.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Projet_Partie_ClientManager.Controllers
+{
+    public class ClientController : Controller
+    {
+        ClientManager clientManager = new ClientManager();
+
+        public bool AjouterClient(Client client)
+        {
+            return clientManager.AjouterClient(client);
+        }
+
+        public List<Client> ConsulterClients()
+        {
+            return clientManager.ConsulterClients();
+        }
+
+        public Client ModifierClient(Client client)
+        {
+            return clientManager.ModifierClient(client);
+        }
+
+        public bool SupprimerClient(Client client)
+        {
+            return clientManager.SupprimerClient(client);
+        }
+
+        public Client RechercherClient(String nom)
+        {
+            return clientManager.RechercherClient(nom);
+        }
+    }
+}
---
+++ Projet-Partie-ClientManager/Projet-Partie-ClientManager/Models/ClientManager.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Projet_Partie_ClientManager.Models
+{
+    public class ClientManager
+    {
+
+        /*
+         * Méthode qui permet d'ajouter un 
+         * nouveau client dans la base de données.
+         * Cette méthode renvoie true si l'insertion
+         * a été effectuer correctement sinon elle
+         * renvoie false.
+         * 
+         * */
+        public bool AjouterClient(Client client)
+        {
+            return false;
+        }
+
+
+        /*
+         * Méthode qui permet de renvoyer la 
+         * liste complète des client de la 
+         * base de données
+         * 
+         * */
+        public List<Client> ConsulterClients()
+        {
+            return null;
+        }
+
+
+        /*
+         * Méhtode qui permet de faire la modification 
+         * du client.
+         * Le client modifié sera renvoyé 
+         * */
+        public Client ModifierClient(Client client)
+        {
+            return null;
+        }
+
+
+        /*
+         * Méthode qui permet de supprimer un client
+         * de la base de données. 
+         * Cette méthode renvoie un booléen pour
+         * attester de la bonne réussite de la 
+         * suppressions
+         * */
+        public bool SupprimerClient(Client client)
+        {
+            return false;
+        }
+        
+
+        /*
+         * Méthode qui permet de chercher un client dans la 
+         * base de données et de le retourner
+         * */
+        public Client RechercherClient(String nom)
+        {
+            return null;
+        }
+    }
+}
